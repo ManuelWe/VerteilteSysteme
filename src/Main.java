aafe1b3f8ef56895dec9a38af0b626b99feac1c8
Ovakefali13 <Ovakefali13@web.de>
2019-10-22 13:12:59 +0200

    Update main after merge issues

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
-package src;
-
-public class Main {
-    public static void main(String args[]) {
-        Server server = new Server(5000);
-        Client client = new Client("127.0.0.1", 5000);
+import java.util.Scanner;
+public class Main
+{
+    public static void main(final String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        System.out.println("******************************************************");
+        System.out.println("\tWelcome to the new Server / Client chooser");
+        System.out.println("******************************************************");
+        System.out.println("Would you like to open a server press s or type server");
+        System.out.println("Would you like to open a client press c or type client");
+        System.out.println("******************************************************");
+        System.out.printf("Your input: ");
+        String input = scanner.nextLine();
+        if (input.equals("s") || input.equals("server")) {
+            final Server server = new Server(5000);
+        }
+        else if (input.equals("c") || input.equals("client")) {
+            System.out.println("Please specify the IP the server is on");
+            input = scanner.nextLine();
+            final Client client = new Client(input, 5000);
+        }
+        else {
+            System.out.println("You are to stupid to type a correct letter, please disconnect your head from your body" +
+                    " and let your body search a new brain");
+        }
     }
 }
