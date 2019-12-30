7da354861330a9afe9df46dd11aa07f873b33137
Jan-Nicolai Geistler <Ovakefali13@web.de>
2019-12-30 20:52:04 +0100

    No debugging lines anymore

--- src/main/java/client/Main.java
+++ src/main/java/client/Main.java
@@ -4,30 +4,6 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(final String[] args) {
-		/*// TODO remove
-		System.setOut(new java.io.PrintStream(System.out) {
-			private StackTraceElement getCallSite() {
-				for (StackTraceElement e : Thread.currentThread().getStackTrace())
-					if (!e.getMethodName().equals("getStackTrace") && !e.getClassName().equals(getClass().getName()))
-						return e;
-				return null;
-			}
-
-			@Override
-			public void println(String s) {
-				println((Object) s);
-			}
-
-			@Override
-			public void println(Object o) {
-				StackTraceElement e = getCallSite();
-				String callSite = e == null ? "??"
-						: String.format("%s.%s(%s:%d)", e.getClassName(), e.getMethodName(), e.getFileName(),
-								e.getLineNumber());
-				super.println(o + "\t\tat " + callSite);
-			}
-		});*/
-
 		String dhcpIp = null;
 		final Scanner scanner = new Scanner(System.in);
 		if (args.length == 0) {
